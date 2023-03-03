4cc37441fdf78e06efdf8250dd6338e417450ae7
sivakumar3121 <126681300+sivakumar3121@users.noreply.github.com>
2023-03-03 14:39:47 +0530

    sss

--- src/app/models/Employee.ts
+++ src/app/models/Employee.ts
@@ -3,23 +3,32 @@ export class Employee{
      id:number;
      name:string;
      email:string;
-     phoneNumber:number;
      username: string
-     password: string
-     department: string
-     designation: string
+     phoneNumber:number;
+     department: string;
+     designation: string;
      roles:Set<string>;
+     address:string;
+     location:string;
+     usertype:string;
+     resourceplanner:string;
+     password: string;
+     
     
-    constructor(id: number, name: string, email: string, phoneNumber: number, username: string, password: string, department: string, designation: string, roles : Set<string>){
+    constructor(id: number, name: string, email: string,username: string,phoneNumber: number,department: string, designation: string,roles : Set<string>,address: string, location: string,usertype: string,resourceplanner: string,password: string){
         this.id = id;
         this.name = name;
         this.email = email;
-        this.phoneNumber = phoneNumber;
         this.username = username;
+        this.phoneNumber = phoneNumber;
+        this.department = department;
+        this.designation = designation;
+        this.roles = roles;
+        this.address = address;
+        this.location = location;
+        this.usertype = usertype;
+        this.resourceplanner = resourceplanner;
         this.password = password;
-        this.department = department
-        this.designation = designation
-        this.roles = roles
     }
 
 }
