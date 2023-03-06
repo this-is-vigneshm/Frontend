9bf94ad94604a6d289e662dbfc409472fff466eb
VIGNESH_M <126800411+this-is-vigneshm@users.noreply.github.com>
2023-03-06 13:50:24 +0530

    dfdvdsdv

--- src/app/models/TicketResp.ts
+++ src/app/models/TicketResp.ts
@@ -1,36 +1,54 @@
 export class TicketResp {
     uuid: string;
     title: string;
-    description: string;
-    employeeName: string
-    employeeMail: string
+    description: string
     category: string;
     status: string;
     employeeId: string;
+    employeeName: string
+    employeeMail: string
     employeeDepartment: string;
+    issueTYpe : string
+    assetId : string
     workOrderId : number
     createdBy:string;
     createdTime: number;
     updatedBy: string;
     updatedTime: number;
     timeTaken: number;
-    expand : boolean = false;
+    expand : boolean = false
     expectedCompletionTime: number;
 
-    constructor(uuid: string, title: string, description: string, 
-        employeeName: string, employeeMail: string, category: string,
-        status: string, employeeId: string, employeeDepartment: string,
-        workOrderId : number, createdBy:string, createdTime: number, updatedBy: string,
-        updatedTime: number, timeTaken: number, expectedCompletionTime:number) {
+
+    constructor(uuid: string,
+        title: string,
+        description: string,
+        category: string,
+        status: string,
+        employeeId: string,
+        employeeName: string,
+        employeeMail: string,
+        employeeDepartment: string,
+        issueTYpe : string,
+        assetId : string,
+        workOrderId : number,
+        createdBy:string,
+        createdTime: number,
+        updatedBy: string,
+        updatedTime: number,
+        timeTaken: number,
+        expectedCompletionTime: number) {
         this.uuid= uuid;
         this.title = title;
         this.description = description;
-        this.employeeName = employeeName;
-        this.employeeMail = employeeMail;
         this.category = category;
         this.status = status
         this.employeeId = employeeId
+        this.employeeName = employeeName;
+        this.employeeMail = employeeMail;
         this.employeeDepartment = employeeDepartment
+        this.issueTYpe = issueTYpe
+        this.assetId=assetId
         this.workOrderId = workOrderId
         this.createdBy = createdBy
         this.createdTime = createdTime
