8dfab321f4f8ddcdd2f601e31971a2471914cd47
VIGNESH_M <126800411+this-is-vigneshm@users.noreply.github.com>
2023-03-10 16:47:38 +0530

    asd

--- src/app/components/create-facility/create-facility.component.ts
+++ src/app/components/create-facility/create-facility.component.ts
@@ -27,42 +27,7 @@ export class CreateFacilityComponent implements OnInit {
   addLoc:boolean=true
 
 
-  panels = [
-    {
-      active: true,
-      disabled: false,
-      name: 'This is panel header 1',
-      customStyle: {
-        background: '#f7f7f7',
-        'border-radius': '4px',
-        'margin-bottom': '24px',
-        border: '0px'
-      }
-    },
-    {
-      active: false,
-      disabled: true,
-      name: 'This is panel header 2',
-      icon: 'double-right',
-      customStyle: {
-        background: '#f7f7f7',
-        'border-radius': '4px',
-        'margin-bottom': '24px',
-        border: '0px'
-      }
-    },
-    {
-      active: false,
-      disabled: false,
-      name: 'This is panel header 3',
-      customStyle: {
-        background: '#f7f7f7',
-        'border-radius': '4px',
-        'margin-bottom': '24px',
-        border: '0px'
-      }
-    }
-  ];
+ 
   ngOnInit(): void {
     this.validateForm = this.fb.group({
       facilityCode: [
