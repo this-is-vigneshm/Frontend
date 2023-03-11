8ff0968dc993830913bb3be317864cbb8375a266
VIGNESH_M <126800411+this-is-vigneshm@users.noreply.github.com>
2023-03-11 18:26:17 +0530

    fds

--- src/app/models/Building.ts
+++ src/app/models/Building.ts
@@ -2,9 +2,11 @@ export class Building {
     id : number 
     name: string
     locationId : number
-    constructor(id : number, name: string, locationId : number){
+    expand : boolean;
+    constructor(id : number, name: string, locationId : number,expand : boolean){
         this.id = id
         this.name = name
         this.locationId = locationId
+        this.expand = expand
     }
 }
